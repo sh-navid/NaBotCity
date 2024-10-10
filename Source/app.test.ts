0d588640356e702672f620cd4ac010dbcc744499
sh-navid <shamshirsaz.navid@live.com>
2024-10-10 09:52:53 +0330

    [Feature] Setup tests

---
+++ Source/app.test.ts
@@ -0,0 +1,5 @@
+describe("Sum two numbers", () => {
+  test("adds 2 + 2 to equal 4", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: "/(Tests|Source)/.*\\.(test|spec)?\\.(ts|tsx)$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
