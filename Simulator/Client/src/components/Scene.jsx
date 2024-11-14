d68e664183d4c22b3a48c8e6dd8efb65bbb8e18f
sh-navid <shamshirsaz.navid@live.com>
2024-11-14 11:05:58 +0330

    [Feature] Include HRobot in scene

--- Simulator/Client/src/components/Scene.jsx
+++ Simulator/Client/src/components/Scene.jsx
@@ -1,4 +1,5 @@
 import { Robot } from "../objects/Robot";
+import { HRobot } from "../objects/HRobot";
 import { Base, Floor } from "./LoadObject";
 import { Canvas } from "@react-three/fiber";
 import { Physics } from "@react-three/rapier";
@@ -13,9 +14,7 @@ const FullCanvasStyle = Object.freeze({
   border: "2px dotted black",
 });
 
-
 const Scene = () => {
-
   return (
     <Canvas
       className="cursor-pointer"
@@ -28,21 +27,25 @@ const Scene = () => {
         <pointLight position={[10, 10, 10]} intensity={100} />
         <OrbitControls autoRotate={true} enableZoom={true} enablePan={true} />
 
-         <Robot initPosition={[4, 0, 4]} name="Robot2" />
-        <Robot initPosition={[4, 0, -4]} name="Robot3" />
-        <Robot initPosition={[-4, 0, 4]} name="Robot4" />
-        <Robot initPosition={[-4, 0, -4]} />
-        <Robot initPosition={[8, 0, 8]} name="Robot2" />
-        <Robot initPosition={[8, 0, -8]} name="Robot3" />
-        <Robot initPosition={[-8, 0, 8]} name="Robot4" />
-        <Robot initPosition={[-8, 0, -8]} /> 
+        {/* <Robot initPosition={[4, 0, 4]} name="Robot2" /> */}
+        {/* <Robot initPosition={[4, 0, -4]} name="Robot3" /> */}
+        {/* <Robot initPosition={[-4, 0, 4]} name="Robot4" /> */}
+        {/* <Robot initPosition={[-4, 0, -4]} /> */}
+        {/* <Robot initPosition={[8, 0, 8]} name="Robot2" /> */}
+        {/* <Robot initPosition={[8, 0, -8]} name="Robot3" /> */}
+        {/* <Robot initPosition={[-8, 0, 8]} name="Robot4" /> */}
+        {/* <Robot initPosition={[-8, 0, -8]} /> */}
 
         <Base initPosition={[0, 0, -18]} initRotation={[0, -Math.PI / 2, 0]} />
         <Base initPosition={[-18, 0, 0]} initRotation={[0, 0, 0]} />
         <Base initPosition={[0, 0, 18]} initRotation={[0, Math.PI / 2, 0]} />
         <Base initPosition={[18, 0, 0]} initRotation={[0, Math.PI, 0]} />
 
-        <Robot initPosition={[0, 0, 0]} isManual={true} />
+        {/* <Robot initPosition={[0, 0, 0]} isManual={true} /> */}
+
+        <HRobot initPosition={[2, 0, 2]} isManual={true} />
+
+        {/* <HRobot15/> */}
 
         <Floor />
       </Physics>
