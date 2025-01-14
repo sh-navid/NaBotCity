e7c536de4763ea28e831aa08e30a6c1d4a04b8a5
sh-navid <shamshirsaz.navid@live.com>
2025-01-14 11:48:06 +0330

    [Revision] 242eb4a

--- Simulator/Client/src/objects/Part.jsx
+++ Simulator/Client/src/objects/Part.jsx
@@ -1,30 +1,53 @@
-import { useGLTF } from "@react-three/drei";
+import { forwardRef, useEffect, useRef } from "react";
 import { Endpoints } from "../configs/Endpoints";
+import { useGLTF } from "@react-three/drei";
 
-export const Part = ({ children, name, position, rotation }) => {
-  const { scene, isLoading, error } = useGLTF(Endpoints.Download(name));
+export const Part = forwardRef(
+  ({ children, name, position, rotation }, ref) => {
+    const { scene, isLoading, error } = useGLTF(Endpoints.Download(name));
 
-  if (isLoading) return null;
-  if (error)
+    if (isLoading) return null;
+    if (error)
+      return (
+        <mesh position={position} rotation={rotation} ref={ref}>
+          <boxBufferGeometry />
+          <meshStandardMaterial color="red" />
+          {children}
+        </mesh>
+      );
+
     return (
-      <mesh position={position} rotation={rotation}>
-        <boxBufferGeometry />
-        <meshStandardMaterial color="red" />
+      <primitive
+        position={position}
+        rotation={rotation}
+        ref={ref}
+        object={scene.clone()}
+      >
         {children}
-      </mesh>
+      </primitive>
     );
-
-  return (
-    <primitive position={position} rotation={rotation} object={scene.clone()}>
-      {children}
-    </primitive>
-  );
-};
+  }
+);
 
 export const Motor = ({ children, position, rotation, angle }) => {
+  const motorRef = useRef();
+  const shaftRef = useRef();
+
+  useEffect(() => {
+    if (motorRef.current) {
+      const shaft = motorRef.current.getObjectByName("Shaft");
+      if (shaft) {
+        shaft.rotation.x = angle;
+        if (shaftRef.current) {
+          shaft.add(shaftRef.current);
+        }
+      }
+    }
+  }, [angle, children]);
+
   return (
-    <Part name={"Motor"} position={position} rotation={rotation}>
-      {children}
+    <Part name={"Motor"} position={position} rotation={rotation} ref={motorRef}>
+      <group ref={shaftRef}>{children}</group>
     </Part>
   );
 };
