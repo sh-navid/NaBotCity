875c8a9be2744711cc2fa8549666e2c67111fc7d
sh-navid <shamshirsaz.navid@live.com>
2025-06-25 19:34:11 +0330

    [Feature] Setup desktop window

---
+++ Desktop/src/main.js
@@ -0,0 +1,46 @@
+/*[src/main.js]*/
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 700,
+    height: 500,
+    frame: true,
+    transparent: false,
+    backgroundColor: "#00000000",
+    icon: path.join(__dirname, 'assets/logo192.png'), // Add the path to your logo here
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadURL("http://localhost:4411");
+
+  // Add an event listener for the Escape key
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.executeJavaScript(`
+      document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+          window.close(); // Close the window
+        }
+      });
+    `);
+  });
+}
+
+// Application lifecycle events
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
