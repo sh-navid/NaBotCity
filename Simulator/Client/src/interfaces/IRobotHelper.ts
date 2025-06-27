6f222bb6e6630009ce4d11634104584a2ad383d9
sh-navid <shamshirsaz.navid@live.com>
2025-06-27 08:21:17 +0330

    [Enhancement] Remove unused files

--- Simulator/Client/src/interfaces/IRobotHelper.ts
+++
@@ -1,7 +0,0 @@
-export interface IRobotHelper {
-  resetArmA(robotData: any): void;
-  resetArmB(robotData: any): void;
-  checkInBound(no: number, pos: number): number;
-  moveArmA(robotData: any, no: number, pos: number, mv: number): void;
-  moveArmB(robotData: any, no: number, pos: number, mv: number): void;
-}
